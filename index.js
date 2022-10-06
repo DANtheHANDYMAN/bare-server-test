697afb392e6f8225aa56b41cce01f01d6f299e1e
DANtheHANDYMAN <dday.dwight@gmail.com>
2022-10-06 18:36:23 -0500

    new chnages

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-(async() => {
-  await import('./index.mjs');
-  // await import("./static/users_test.js");
-})();
+// (async() => {
+//   await import('./index.mjs');
+//   // await import("./static/users_test.js");
+// })();
 
 
-// ! https://andrewbaisden.medium.com/how-to-deploy-a-node-express-app-to-vercel-6fa567a041e2
+// // ! https://andrewbaisden.medium.com/how-to-deploy-a-node-express-app-to-vercel-6fa567a041e2
+
+
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
