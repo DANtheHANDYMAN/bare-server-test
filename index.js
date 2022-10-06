be1dde3b293e1509c51f2cd9b146466acc2f73bb
DANtheHANDYMAN <dday.dwight@gmail.com>
2022-10-06 18:38:00 -0500

    efsfsef

--- index.js
+++ index.js
@@ -8,14 +8,23 @@
 
 
 
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
