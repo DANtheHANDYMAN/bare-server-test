ef975ff106c36cb5184cacbac99c85b871147946
DANtheHANDYMAN <dday.dwight@gmail.com>
2022-10-06 18:33:04 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => res.send("Home Page Route"));
+// app.get("/about", (req, res) => res.send("About Page Route"));
+// app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+// app.get("/contact", (req, res) => res.send("Contact Page Route"));
+// const port = process.env.PORT || 3000;
+// app.listen(port, () =>
+//   console.log(`Server running on ${port}, http://localhost:${port}`)
+// );
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- index.js
+++ index.js
@@ -2,3 +2,6 @@
   await import('./index.mjs');
   // await import("./static/users_test.js");
 })();
+
+
+// ! https://andrewbaisden.medium.com/how-to-deploy-a-node-express-app-to-vercel-6fa567a041e2
